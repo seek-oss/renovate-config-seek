be4ac60c4df58ebd03f33b322b1c971ef28db745
michaeltaranto <michael.taranto@gmail.com>
2018-01-08 10:36:06 +1100

    Add validation, tests, and documentation

---
+++ test.js
@@ -0,0 +1,12 @@
+const assert = require('assert');
+const { validateConfig } = require('renovate/lib/config/validation');
+const pkgJson = require('./package.json');
+
+const config = pkgJson['renovate-config'];
+
+Object.keys(config).forEach(presetName => {
+  const { errors, warnings } = validateConfig(config[presetName]);
+
+  assert(errors.length === 0, `Preset '${presetName}' contains errors:\n\n${JSON.stringify(errors, null, 2)}`);
+  assert(warnings.length === 0, `Preset '${presetName}' contains errors:\n\n${JSON.stringify(warnings, null, 2)}`);
+});
