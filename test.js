7a3b1988a06e2717e42e4cfbf91d21da2cd3c781
Mark Dalgleish <mark.john.dalgleish@gmail.com>
2018-01-08 12:47:43 +1100

    test(warnings): Fix assertion message for warnings (#3)

--- test.js
+++ test.js
@@ -8,5 +8,5 @@ Object.keys(config).forEach(presetName => {
   const { errors, warnings } = validateConfig(config[presetName]);
 
   assert(errors.length === 0, `Preset '${presetName}' contains errors:\n\n${JSON.stringify(errors, null, 2)}`);
-  assert(warnings.length === 0, `Preset '${presetName}' contains errors:\n\n${JSON.stringify(warnings, null, 2)}`);
+  assert(warnings.length === 0, `Preset '${presetName}' contains warnings:\n\n${JSON.stringify(warnings, null, 2)}`);
 });
